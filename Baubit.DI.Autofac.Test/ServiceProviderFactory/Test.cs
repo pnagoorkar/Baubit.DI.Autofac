fa881fd390c3d2a36ecc14bad161579b822d40a4
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-12-06 19:22:39 -0500

    WIP - unit tests

---
+++ Baubit.DI.Autofac.Test/ServiceProviderFactory/Test.cs
@@ -0,0 +1,9 @@
+﻿namespace Baubit.DI.Autofac.Test.ServiceProviderFactory
+{
+    /// <summary>
+    /// Unit tests for <see cref="Baubit.DI.Autofac.ServiceProviderFactory"/>
+    /// </summary>
+    public class Test
+    {
+    }
+}
